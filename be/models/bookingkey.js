20e985172f5c9286ca1076848941b74edc23437b
moradop.h <moradop1201@gmail.com>
2025-06-16 09:50:45 +0700

    initial frontend

--- be/models/bookingkey.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const bookingKeySchema = new mongoose.Schema({
-  room: Number,
-  id: String,
-  key: String
-}, { timestamps: true });
-
-module.exports = mongoose.model('bookingkey', bookingKeySchema);
--- be/routes/pins.routes.js
+++
@@ -1,7 +0,0 @@
-// const express = require("express");
-// const {  } = require("../controllers/pins.controllers");
-// const Userrouter = express.Router();
-
-// Userrouter.get("/", );
-
-// module.exports = Userrouter;
