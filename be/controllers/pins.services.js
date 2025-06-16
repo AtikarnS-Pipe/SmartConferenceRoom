1483ea357bcf82ae22d01539618bb8f595ed93e4
moradop.h <moradop1201@gmail.com>
2025-06-16 09:52:39 +0700

    initial admin

--- be/controllers/pins.services.js
+++
@@ -1,1 +0,0 @@
-const { fetchAllRoom } = require("./admin.controllers");
--- be/routes/users.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { getuser } = require("../controllers/users.controllers");
-const Userrouter = express.Router();
-
-Userrouter.get("/sse/:floors/:rooms", getuser);
-
-module.exports = Userrouter;
