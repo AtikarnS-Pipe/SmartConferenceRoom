e9e3bbf8ae491d8d8d57bdfbfe3cc4720c490873
moradop.h <moradop1201@gmail.com>
2025-06-16 09:47:14 +0700

    initial backend

--- fe/src/components/light-container.jsx
+++
@@ -1,7 +0,0 @@
-export default function LightContainer({ isOccupied }) {
-  return (
-    <div className="light-container">
-      <div className={`light-indicator ${isOccupied ? 'occupied' : 'available'}`}></div>
-    </div>
-  );
-}
