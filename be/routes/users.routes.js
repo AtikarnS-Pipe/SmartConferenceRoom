71a3ec477299d309e57fbc89ee57ac947fa47b1a
Seen <you@example.com>
2025-06-17 10:50:46 +0700

    handle wtf situation brother

--- be/routes/users.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getuser, keyPins } = require("../controllers/users.controllers");
-const Userrouter = express.Router();
-
-Userrouter.get("/sse/:floors/:rooms", getuser);
-Userrouter.post("/validatePin", keyPins);
-
-module.exports = Userrouter;
