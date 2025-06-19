cbedcb222ae5c62ee10c30fd2e7978fb9d3000d0
moradop.h <moradop1201@gmail.com>
2025-06-19 16:45:13 +0700

    add tokenCahche and encodingTool

---
+++ be/utils/encode.js
@@ -0,0 +1,31 @@
+const crypto = require('crypto');
+
+const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY || crypto.randomBytes(32); 
+const IV_LENGTH = 12; 
+
+// Encrypt
+function encryptToken(plainText) {
+  const iv = crypto.randomBytes(IV_LENGTH);
+  const cipher = crypto.createCipheriv('aes-256-gcm', ENCRYPTION_KEY, iv);
+
+  const encrypted = Buffer.concat([cipher.update(plainText, 'utf8'), cipher.final()]);
+  const tag = cipher.getAuthTag();
+
+  return Buffer.concat([iv, tag, encrypted]).toString('base64');
+}
+
+// Decrypt
+function decryptToken(encryptedBase64) {
+  const data = Buffer.from(encryptedBase64, 'base64');
+  const iv = data.slice(0, IV_LENGTH);
+  const tag = data.slice(IV_LENGTH, IV_LENGTH + 16);
+  const encryptedText = data.slice(IV_LENGTH + 16);
+
+  const decipher = crypto.createDecipheriv('aes-256-gcm', ENCRYPTION_KEY, iv);
+  decipher.setAuthTag(tag);
+
+  const decrypted = decipher.update(encryptedText, null, 'utf8') + decipher.final('utf8');
+  return decrypted;
+}
+
+module.exports = { encryptToken, decryptToken };
