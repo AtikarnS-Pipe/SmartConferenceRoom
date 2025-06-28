6967cfdbbca4ef11209b5d15a3f7023ecb7df332
moradop.h <moradop1201@gmail.com>
2025-06-28 18:26:58 +0700

    create LoginLogSchema

---
+++ be/models/LoginLog.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const LoginLogSchema = new mongoose.Schema({
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ['admin', 'housekeeper'],
+  },
+  login_time: {
+    type: Date,
+    required: true,
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('LoginLog', LoginLogSchema);
