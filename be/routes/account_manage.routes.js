d055a9babf6a285c6224ef912fde37ef2d1ef4ca
Seen <seengamer2547@gmail.com>
2025-07-02 09:45:03 +0700

    Handle merge confict

--- be/routes/account_manage.routes.js
+++ be/routes/account_manage.routes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { Auth,
     Createhousekeeper,
     createadmin,
@@ -8,14 +7,10 @@ const { Auth,
     deletehousekeeper,
     editpinhousekeeper
  } = require('../controllers/account.controllers');
-=======
-const { Auth, Createhousekeeper, createadmin, refreshadmintoken, signout, sendEmailOTP, verifyEmailOTP, resetEmailPassword } = require('../controllers/account.controllers');
->>>>>>> d9701b01c7b592f1af8192aa0a3a8b1703b97066
 const { authorize } = require('../middlewares/auth.middleware')
 const express = require("express");
 const AccountRouter = express.Router();
 
-<<<<<<< HEAD
 Accountrouter.post('/createadmin', createadmin);
 Accountrouter.post('/auth', Auth);
 Accountrouter.post('/changeadminpw', authorize, ChangeAdminPW);
@@ -24,15 +19,5 @@ Accountrouter.post('/edithousekeeper', authorize, editpinhousekeeper);
 Accountrouter.post('/deletehousekeeper', authorize, deletehousekeeper);
 Accountrouter.post('/refreshtoken', refreshadmintoken);
 Accountrouter.post('/signout', authorize, signout);
-=======
-AccountRouter.post('/createadmin', createadmin);
-AccountRouter.post('/auth', Auth);
-AccountRouter.post('/createhousekeeper', authorize, Createhousekeeper);
-AccountRouter.post('/refreshtoken', refreshadmintoken);
-AccountRouter.post('/otp/send', sendEmailOTP);
-AccountRouter.post('/otp/verify', verifyEmailOTP);
-AccountRouter.post('/otp/reset', resetEmailPassword);
-// Accountrouter.post('/signout', authorize, signout);
->>>>>>> d9701b01c7b592f1af8192aa0a3a8b1703b97066
 
 module.exports = AccountRouter;
