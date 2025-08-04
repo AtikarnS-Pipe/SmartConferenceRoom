860c776f3a1c24678f7a08231e563dcd48672de1
Chitsanuchat.a <chitsanuchat.a@tcc-technology.com>
2025-08-04 17:48:37 +0700

    edit button dark

--- Admin/src/Component/Manage/Superadmin.jsx
+++ Admin/src/Component/Manage/Superadmin.jsx
@@ -666,22 +666,23 @@ const performDeleteAdmins = async () => {
               <p className={`${darkMode ? 'text-gray-400' : 'text-gray-600'} mt-1`}>Manage users and permission</p>
             </div>
             <div className="flex items-center gap-3">
+              {/* Dark Mode Toggle */}
+              <RefreshButton title={darkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}>
+                {darkMode ? (
+                    <div onClick={toggleDarkMode}   className="bg-gray-200 p-2 rounded-full">
+                      <Sun className="w-4 h-4" />
+                    </div>
+                  ) : (
+                    <div onClick={toggleDarkMode} className="bg-gray-400 p-2 rounded-full">
+                      <Moon className="w-4 h-4 text-gray" />
+                    </div>
+                  )}
+              </RefreshButton>
                 <div className='flex px-4 py-1.5 gap-2 rounded-lg bg-yellow-600 text-white'>
                     <Crown  className="w-4 h-6" />
                    <h1>{profile?.role}</h1>
                 </div>
-              {/* Dark Mode Toggle */}
-              <RefreshButton title={darkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}>
-                {darkMode ? (
-                    <div onClick={toggleDarkMode}   className="bg-blue-600 p-2 rounded-full">
-                      <Sun className="w-4 h-4" />
-                    </div>
-                  ) : (
-                    <div onClick={toggleDarkMode} className="bg-blue-400 p-2 rounded-full">
-                      <Moon className="w-4 h-4" />
-                    </div>
-                  )}
-              </RefreshButton>
+              
 
               {/* PIN Display */}
               <div className={`flex items-center gap-2 px-4 py-1.5 rounded-lg w-fit ${
