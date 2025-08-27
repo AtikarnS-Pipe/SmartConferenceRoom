4bae44631a13958ec05743fd92d6059700eae387
nareupol.a <Nareupol.A@tcc-technology.com>
2025-08-27 13:45:08 +0700

    add path /api1 and /api2 for nginx

--- be/app.js
+++ be/app.js
@@ -24,10 +24,10 @@ app.use(cookieParser());
 
 monitorToken(); // เริ่ม monitor token loop
 
-app.use("/admin", Adminrouter);
-app.use("/user", Userrouter);
-app.use("/account", Accountrouter);
-app.use("/superadmin", SuperAdminRouter)
+app.use("/api1/admin", Adminrouter);
+app.use("/api2/user", Userrouter);
+app.use("/api1/account", Accountrouter);
+app.use("/api1/superadmin", SuperAdminRouter)
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Smart Display Conference System!');
